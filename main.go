5fbd3c8317ff76427b34ac7930fb44bc4a91e8ea
hendisantika <hendisantika@yahoo.co.id>
2025-03-03 10:35:10 +0700

    feat: Add Test

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ func main() {
 	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
 	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
 	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	fmt.Printf("Hello and welcome, %s!", s)
 
 	for i := 1; i <= 5; i++ {
 		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
 		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
 		fmt.Println("i =", 100/i)
 	}
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
 }
---
+++ main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := add(2, 3)
+	expected := 5
+	if result != expected {
+		t.Errorf("add(2, 3) = %d; want %d", result, expected)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	result := Subtract(5, 3)
+	expected := 2
+	if result != expected {
+		t.Errorf("Subtract(5, 3) = %d; want %d", result, expected)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	result := Multiply(2, 3)
+	expected := 6
+	if result != expected {
+		t.Errorf("Multiply(2, 3) = %d; want %d", result, expected)
+	}
+}
